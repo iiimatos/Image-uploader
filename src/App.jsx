75e67bc86a76682410fd2bca151eb37e37a3be21
iiimatos <ivanmatosvillar@gmail.com>
2022-06-29 22:50:35 -0400

    install and configuration of tailwind and create the first component

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,23 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import UploadImage from "./components/UploadImage";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
-}
+    <section className="flex justify-center items-center flex-col bg-gray-100 h-screen">
+      <UploadImage />
+      <footer className="text-slate-400 mt-10">
+        Created by username{" "}
+        <a
+          href="https://github.com/iiimatos"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="font-bold"
+        >
+          iiimatos
+        </a>{" "}
+        - devchallenges.io
+      </footer>
+    </section>
+  );
+};
 
-export default App
+export default App;
